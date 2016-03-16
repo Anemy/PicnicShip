9f746700e64232daf7fd04344f2cb062bb1464ad
Rhys Howell <rhysh@live.com>
2016-03-16 13:08:46 -0400

    Got basic game loop and logic started

---
+++ public/constants.js
@@ -0,0 +1,28 @@
+/*
+
+@author Rhys
+
+This holds all of the constants used by the battleship game
+
+*/
+
+// to differentiate the boards
+const LEFT = -1;
+const RIGHT = 1;
+
+// hold the indexes of the type of ship and name
+// squares on the board will hold an index of this array if they have a ship
+const ships = [
+  {name: 'Carrier', size: 5},
+  {name: 'Battleship', size: 4},
+  {name: 'Cruiser', size: 3},
+  {name: 'Submarine', size: 3},
+  {name: 'Destroyer', size: 2},
+];
+
+const boardSize = 10;
+
+// To differentiate locations on the map
+const EMPTY = -1;
+
+console.log('Constants loaded.')
--- public/script.js
+++ public/script.js
@@ -1,3 +1,48 @@
+/*
 
+@author Rhys
 
-console.log('Script loaded.');
+*/
+
+// boards which contain the game state
+var leftBoard = [];
+var rightBoard = [];
+
+// holds the set interval of the game loop
+var gameLoopInterval;
+
+// left player gets the first move
+var currentTurn = LEFT;
+
+// resets the state of the game to blank (no ships)
+function resetGame() {
+	leftBoard = [];
+	rightBoard = [];
+
+	for(var i = 0; i < boardSize; i++) {
+		leftBoard[i] = [];
+		rightBoard[i] = [];
+
+		for(var k = 0; k < boardSize; k++){
+			leftBoard[i][k] = EMPTY;
+			rightBoard[i][k] = EMPTY;
+		}
+	}
+}
+
+function drawGame() {
+
+}
+
+function gameLoop() {
+
+}
+
+// calls to reset the game and start the game loop
+function startGame() {
+	resetGame();
+
+	gameLoopInterval = setInterval(gameLoop, gameSpeed);
+}
+
+console.log('Script loaded. ');
