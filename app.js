616b1bfeeff8db208b63de838e401bfd41f820d4
Rhys Howell <rhysh@live.com>
2016-03-16 12:47:25 -0400

    Basic webpage sending and script

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 'use strict';
 
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
 // Serve assets in /public.
 app.use(express.static(__dirname + '/public'));
-
-// Allow POSTing JSON.
-app.use(bodyParser.json());
 
 // Root URL repsonds to HTTP GET, serves index.html file
 app.get('/', function(req, res) {
@@ -17,12 +13,3 @@ app.get('/', function(req, res) {
 
 // Express Server listens on port 5000
 app.listen(process.env.PORT || 5000);
-
-function autocomplete(query) { 
-  return ['hi'];
-}
-
-app.post('/query', function(req, res) {
-  var query = req.body.query;
-  res.json(autocomplete(query));
-});
--- public/script.js
+++ public/script.js
@@ -1,18 +1,3 @@
-$('input').on('input', function() {
-	$.ajax({
-		method: 'post',
-		url: '/query',
-		contentType: 'application/json',
-		data: JSON.stringify({
-			query: $(this).val()
-		})
-	})
-		.success(function(results) {
-			$('.results').empty();
 
-			results.forEach(function(result) {
-				var li = $('<li>').text(result);
-				$('.results').append(li);
-			});
-		});
-});
+
+console.log('Script loaded.');
